20a10c9fe79ac030c959190b559dfdbca1546410
Rob Lauer <rlauer6@comcast.net>
2022-06-07 03:52:35 -0400

    perltidy, perlcritic, force array (item)

--- src/examples/DescribeInstances.pl
+++ src/examples/DescribeInstances.pl
@@ -1,83 +1,101 @@
-#!/usr/bin/perl
+#!/usr/bin/env perl
+
+package Amazon::CLI::EC2;
 
 use strict;
 use warnings;
 
 use Amazon::EC2;
 use Data::Dumper;
+use English qw{ -no_match_vars };
 use Term::ANSIColor;
 use Text::ASCIITable;
 use Furl;
- 
-my $ec2 = new Amazon::EC2({debug => 0, user_agent => new Furl});
+use Data::Dumper;
+use Carp;
+use Carp::Always;
 
-my $result = eval {
-  $ec2->DescribeInstances;
-};
+use ReadonlyX;
 
-if ( $@ && ref($@) =~/API::Error/ ) {
-  print Dumper([ $@->get_response, $@->get_error ])
-}
-elsif ($@) {
-  die $@;
-}
+Readonly our $TRUE  => 1;
+Readonly our $FALSE => 0;
 
-# print STDERR Dumper [ $result ];
+Readonly our $EMPTY     => q{};
+Readonly our $OCTOTHORP => q{#};
 
-my $t = new Text::ASCIITable({headingText => 'EC2 Instances', allowANSI => 1});
-$t->setCols('#', 'instanceId', 'name', 'imageId', 'instanceType', 'privateIpAddress', 'launchTime', 'instanceState');
+Readonly::Hash our %COLORS => (
+  running    => 'green',
+  terminated => 'red',
+  stopped    => 'magenta',
+  pending    => 'yellow',
+);
 
-my @data;
+our $VERSION = '0.01';
 
-foreach my $item (@{$result->{reservationSet}->{item}}) {
-  push @data, $item->{instancesSet}->{item};
-}
+caller or __PACKAGE__->main();
 
-# sort by state (running)
-@data = sort { $b->{instanceState}->{name} eq 'running' ? 1 : -1 } @data;
+########################################################################
+sub main {
+########################################################################
 
-my $row = 0;
+  my $ec2 = Amazon::EC2->new( { user_agent => Furl->new } );
 
-foreach my $item (@data) {
-  my $state = $item->{instanceState}->{name};
+  my $result = eval { return $ec2->DescribeInstances; };
 
-  my $name = $item->{tagSet}->{item}->{Name};
-  if ( $name ) {
-    $item->{name} = $name->{value};
+  if ( $EVAL_ERROR && ref($EVAL_ERROR) =~ /API::Error/xsm ) {
+    print Dumper( [ $EVAL_ERROR->get_response, $EVAL_ERROR->get_error ] );
   }
-  else {
-    if ( exists $item->{tagSet}->{tagSet}->{item}->{key} && $item->{tagSet}->{item}->{key} eq 'Name' ) {
-      $item->{name} = $item->{tagSet}->{item}->{value};
+  elsif ($EVAL_ERROR) {
+    croak $EVAL_ERROR;
+  }
+
+  my $t = Text::ASCIITable->new(
+    { headingText => 'EC2 Instances', allowANSI => $TRUE } );
+
+  $t->setCols( $OCTOTHORP, 'instanceId', 'name', 'imageId',
+    'instanceType', 'privateIpAddress', 'launchTime', 'instanceState' );
+
+  my @data;
+
+  foreach my $item ( @{ $result->{'reservationSet'}->{'item'} } ) {
+    push @data, $item->{'instancesSet'}->{'item'}->[0];
+  }
+
+  # sort by state (running)
+  @data
+    = sort { $a->{'instanceState'}->{'name'} eq 'running' ? -1 : 1 } @data;
+
+  my $row = 0;
+
+  foreach my $item (@data) {
+    my $state = $item->{'instanceState'}->{'name'};
+
+    my $name = $item->{'tagSet'}->{'item'}->{'Name'};
+
+    if ($name) {
+      $item->{'name'} = $name->{'value'};
     }
-  }
-  
-  my $color;
-  
-  for ("$state") {
-    
-    /running/ && do {
-      $color = "green";
-      last;
-      };
-      
-    /terminated/ && do {
-      $color = "red";
-      last;
-    };
-    
-    /stopped/ && do {
-      $color = "magenta";
-      last;
-    };
-    
-    /pending/ && do {
-      $color = "yellow";
-      last;
-    };
-  }
+    else {
+      if ( exists $item->{'tagSet'}->{'item'}->{'key'}
+        && $item->{'tagSet'}->{'item'}->{'key'} eq 'Name' ) {
+        $item->{'name'} = $item->{'tagSet'}->{'item'}->{'value'};
+      }
+    } ## end else [ if ($name) ]
 
-  $t->addRow(++$row, @{$item}{qw/instanceId name imageId instanceType privateIpAddress launchTime/}, colored($state, $color));
-}
+    my $color = $COLORS{$state} // 'red';
 
-print $t;
+    $t->addRow(
+      ++$row,
+      @{$item}{
+        qw{ instanceId name imageId instanceType privateIpAddress launchTime}
+      },
+      colored( $state, $color )
+    );
+  } ## end foreach my $item (@data)
 
+  print $t;
+
+  exit;
+} ## end sub main
+
+1;
